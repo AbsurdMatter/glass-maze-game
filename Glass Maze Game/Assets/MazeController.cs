7ee440bd1d5dce44c98debce3066fdfdd460d0b6
Absurd Matter <urska.weiss@gmail.com>
2019-12-07 13:55:57 +0100

    Added player movement and camera rotation

---
+++ Glass Maze Game/Assets/MazeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float moveSpeedModifier = 0.5f;
+    float dirX, dirY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        dirX = Input.acceleration.x * moveSpeedModifier;
+        dirY = Input.acceleration.y * moveSpeedModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(rb.velocity.x + dirX, rb.velocity.y + dirY);
+    }
+}
---
+++ Glass Maze Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+    }
+    void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHoriz, 0.0f, moveVert);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
